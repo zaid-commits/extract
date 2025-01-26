ec18c197762c68e1608852946ef8fd6d1d37f0c1
Zaid Rakhange <zaidcommits.github@gmail.com>
2025-01-26 18:16:54 +0530

    Update app.run configuration to use environment variable for port

--- main.py
+++ main.py
@@ -55,4 +55,5 @@ def chat():
     return jsonify({'response': response_text})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
